99f9bee27cf83d976a0f71bb21f86196d189a2f9
Rafael Santos <rafa.can.dev@gmail.com>
2024-01-20 21:14:34 -0500

    Comentarios no Tutorial11Test

--- src/test/java/aprenda/jpa/tutorial/Tutorial11Test.java
+++ src/test/java/aprenda/jpa/tutorial/Tutorial11Test.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 
+import java.util.stream.IntStream;
+
 import static java.util.stream.IntStream.rangeClosed;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,13 +31,15 @@ public class Tutorial11Test {
 
     @Test
     void paginacao() {
-        rangeClosed(0, 4).forEach(i -> {
+        // Poderia ser um for loop, mas estou usando o IntStream pelo 'cool factor'
+        IntStream.rangeClosed(0, 4).forEach(i -> {
             var item = new Item();
             item.setNome("item_" + i);
             itemRepository.save(item);
         });
         assertEquals(5, itemRepository.count());
         var pagina_0 = itemRepository.findAll(PageRequest.ofSize(3));
+        assertEquals(3, pagina_0.getNumberOfElements());
         assertEquals("item_0", pagina_0.getContent().get(0).getNome());
         assertEquals("item_1", pagina_0.getContent().get(1).getNome());
         assertEquals("item_2", pagina_0.getContent().get(2).getNome());
